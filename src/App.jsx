05a661dfce5693447dd972775156418d123b037b
andreaYr <yuriramirez5800@gmail.com>
2022-03-30 13:50:01 -0500

    se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Intro from './components/Intro.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Intro></Intro>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './intro.css';
+
+const Intro = () => {
+  return (
+    <div className='i'>
+        <div className='i-left'>
+            <div className='i-left-wrapper'>
+                <h2>Hello, My name is</h2>
+                <h1>Yuri Andrea</h1>
+                <div className='i-title'>
+                    <div className='i-title-wrapper'>
+                        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div className='i-right'>right</div>
+
+
+    </div>
+  )
+}
+
+export default Intro;
