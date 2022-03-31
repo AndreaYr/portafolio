c2a40c54607add213de8f0324d95921960e215ec
andreaYr <yuriramirez5800@gmail.com>
2022-03-31 17:55:50 -0500

    se agrega cambios

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,18 +6,31 @@ const Intro = () => {
     <div className='i'>
         <div className='i-left'>
             <div className='i-left-wrapper'>
-                <h2>Hello, My name is</h2>
-                <h1>Yuri Andrea</h1>
+                <h2 className='i-intro'>Hello, My name is</h2>
+                <h1 className='i-name'>Yuri Andrea</h1>
                 <div className='i-title'>
-                    <div className='i-title-wrapper'>
-                        
-                    </div>
+                  <div className='i-title-wrapper'>
+                    <div className='i-title-item '>Web Developer</div>
+                    <div className='i-title-item '>UI/UX Designer</div>
+                    <div className='i-title-item '>Photographer</div>
+                    <div className='i-title-item '>Writer</div>
+                    <div className='i-title-item '>Content Creator</div>
+                      
+                  </div>
                 </div>
 
+              <div className='i-desc'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Animi incidunt reprehenderit odit laudantium quis, harum autem deserunt 
+                  repellendus accusantium sit sed illo laborum dolore voluptas! Iste rerum 
+                  unde deserunt ipsam.</p>
+
+              </div>
             </div>
         </div>
-        <div className='i-right'>right</div>
 
+        <div className='i-right'></div>
+          <img src='' alt='' ></img>
 
     </div>
   )
