0800baf2679065919878a881acbd49b41dcbcf8f
Andrea Reyes <yuriramirez5800@gmail.com>
2022-04-04 01:32:24 -0500

    se agrega mas cambios

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const products = [
+ 
+    {
+        id: 1,
+        img: "",
+        link: "",
+    },
+
+    {
+        id: 2,
+        img: "",
+        link: "",
+    },
+
+    {
+        id: 3,
+        img: "",
+        link: "",
+    }
+]
