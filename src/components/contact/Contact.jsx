7b0c0eceb8bcd678a4eba3b19bf92690d39f62e5
andreaYr <yuriramirez5800@gmail.com>
2022-04-05 14:16:31 -0500

    Se agrega envio de formulario

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './contact.css';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+
+    const formRef = useRef();
+
+    const handleSubmit = (e) =>{
+       e.preventDefault();
+
+       emailjs.sendForm(
+        'service_28tr9qt', 
+        'template_xfom3wk',
+        formRef.current, 
+        'v19y4yH88OtADkVR9'
+        )
+
+       .then((result) => {
+           console.log(result.text);
+       }, (error) => {
+           console.log(error.text);
+       });
+    }
+
   return (
     <div className='c'>
         <div className='c-bg'>
             <div className='c-wrapper'>
                 <div className='c-left'>
-                    <h1 className='c-title'>Contáctame</h1>
+                    <h1 className='c-title'>¡Hablemos!</h1>
                     <p className='c-desc'>Si estás interesado en mi trabajo o tienes alguna inquietud, no dudes en escribirme </p>
-                    <form>
-                        <input type="text" placeholder='Name' name='user_name'/>
-                        <input type="text" placeholder='Subject' name='user_subject'/>
-                        <input type="text" placeholder='Email' name='user_Email'/>
-                        <textarea rows="5" placeholder='Message' name='message'/>
-                        <button>Enviar</button>
+
+                    <form ref={formRef} onSubmit={handleSubmit}>
+                        <label>Tu nombre</label><br/>
+                        <input type="text" placeholder='Nombre' name='user_name'/>
+                        <br/>
+                        <label>Tema</label><br/>
+                        <input type="text" placeholder='Tema' name='user_subject'/>
+                        <br/>
+
+                        <label>Tu correo</label><br/>
+                        <input type="text" placeholder='Correo' name='user_email'/>
+                        <br/>
+
+                        <label>Tu mensaje</label><br/>
+                        <textarea rows="5" placeholder='Mensaje' name='message'/>
+                        <button className='c-button'>Enviar</button>
 
                     </form>
                 </div>
