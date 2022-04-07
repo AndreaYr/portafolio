fbaaee1dad909179279f6390c6266d2fa765f89a
Andrea Reyes <yuriramirez5800@gmail.com>
2022-04-07 07:01:25 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import About from './components/about/About';
 import ProductList from './components/productList/ProductList';
 import Contact from './components/contact/Contact';
 import Toggle from './components/toggle/Toggle';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
+    
      <Toggle/>
       <Intro/>
       <About/>
       <ProductList />
       <Contact/>
+      <Footer/>
     </div>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,42 +3,34 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <div> <div className='c-info'>
-    <div className='c-info-item'>
-        <img 
-            src=''
-            alt=''
-            className='c-icon'
-        />
-        +57 310-643-4580
-    </div>
 
-    <div className='c-info-item'>
-        <img 
-            src=''
-            alt=''
-            className='c-icon'
-        />
-        yuriramirez5800@gmail.com
-    </div>
 
-    <div className='c-info-item'>
-        <img 
-            src=''
-            alt=''
-            className='c-icon'
-        />
-        Armenia-Quindío (Colombia)
+    <div className='f'>
+        <div className='f-info'>
+            <div className='f-info-item'>
+                <h5>Inicio</h5>
+                <p>Acerca de mi</p>
+                <p>Proyectos</p>
+                
+            </div>
+
+            <div className='f-info-item'>
+                <h6>Contáctame</h6>
+                <p>Correo</p>
+            </div>
+
+            <div className='f-info-item'>
+                <h6>Social</h6>
+                    <img 
+                        src=''
+                        alt=''
+                        className='c-icon'
+                    />
+            </div>
+            <hr/>
+        </div>
+       
     </div>
-</div>
-<div className='a-award'>
-              <img src='' alt='' className='a-award-img' />
-              <div  className='a-award-texts'>
-                <h4 className='a-award-title'>Programate (los estilos estan en el about.css)</h4>
-                <p className='a-award-desc'>Estudiante.</p>
-              </div>
-          </div>
-</div>
   )
 }
 
