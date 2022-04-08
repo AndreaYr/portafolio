99012297e612b8c5b70f4eef15085ff9353474c1
Andrea Reyes <yuriramirez5800@gmail.com>
2022-04-08 06:47:59 -0500

    se agrega cambios

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -28,31 +28,30 @@ const Contact = () => {
 
   return (
     <div className='c'>
-        <div className='c-bg'>
-            <div className='c-wrapper'>
+        <div className='c-bg'></div>
                 <div className='c-left'>
                     <h1 className='c-title'>¡Hablemos!</h1>
                     <p className='c-desc'>Si estás interesado en mi trabajo o tienes alguna inquietud, no dudes en escribirme </p>
+                    <div className='c-form'>
+                        <form ref={formRef} onSubmit={handleSubmit}>
+                            <label>Tu nombre</label><br/>
+                            <input type="text" placeholder='Nombre' name='user_name'/>
+                            <br/>
+                            <label>Tema</label><br/>
+                            <input type="text" placeholder='Tema' name='user_subject'/>
+                            <br/>
 
-                    <form ref={formRef} onSubmit={handleSubmit}>
-                        <label>Tu nombre</label><br/>
-                        <input type="text" placeholder='Nombre' name='user_name'/>
-                        <br/>
-                        <label>Tema</label><br/>
-                        <input type="text" placeholder='Tema' name='user_subject'/>
-                        <br/>
+                            <label>Tu correo</label><br/>
+                            <input type="text" placeholder='ejemplo@gmail.com' name='user_email'/>
+                            <br/>
 
-                        <label>Tu correo</label><br/>
-                        <input type="text" placeholder='ejemplo@gmail.com' name='user_email'/>
-                        <br/>
-
-                        <label>Tu mensaje</label><br/>
-                        <textarea rows="5" placeholder='Mensaje' name='message'/>
-                        <button className='c-button'>Enviar</button>
-                        {done && "Mensaje Enviado"}
-                    </form>
-                </div>
-            </div>
+                            <label>Tu mensaje</label><br/>
+                            <textarea rows="5" placeholder='Mensaje' name='message'/>
+                            <button className='c-button'>Enviar</button>
+                            {done && "Mensaje Enviado"}
+                        </form>
+                    </div>
+            
         </div>
     </div>
   )
